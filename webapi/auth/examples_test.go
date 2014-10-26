24efa1de0b621eb40dedb56e9261504f743b2d58
James Wendel <jmwendel@gmail.com>
2014-10-26 12:24:49 -0500

    access_token validation test framework.  pretty awesome.

--- webapi/auth/examples_test.go
+++ webapi/auth/examples_test.go
@@ -22,6 +22,7 @@ type tokenData struct {
 	code        int
 	contentType string
 	// body        string
+	bodyJson map[string]interface{}
 }
 
 func TestOath(t *testing.T) {
@@ -35,9 +36,10 @@ func TestOath(t *testing.T) {
 
 	topcoderUrl := ts.URL + "/api/2/domains/topcoder.com/oauth/access_token"
 
-	td := tokenData{topcoderUrl, "s6BhdRkqt3", "7Fjfp0ZBr1KtDRbnfVdmIw", "client_credentials", 200, "application/json"}
+	bodyJson := map[string]interface{}{"token_type": "bearer", "expires_in": float64(3600), "access_token": nil}
+	td := tokenData{topcoderUrl, "s6BhdRkqt3", "7Fjfp0ZBr1KtDRbnfVdmIw", "client_credentials", 200, "application/json", bodyJson}
 	b, m := runAccessToken(t, td)
-	t.Log("body:", b, "\nmap:", m)
+	at := m["access_token"].(string)
 }
 
 func runAccessToken(t *testing.T, td tokenData) (string, map[string]interface{}) {
@@ -66,6 +68,7 @@ func runAccessToken(t *testing.T, td tokenData) (string, map[string]interface{})
 	body := string(bytes[:])
 	var m map[string]interface{}
 
+	// Lets try to decode the json and compare to the passed in bodyJson
 	if ct == "application/json" {
 		var f interface{}
 		err := json.Unmarshal(bytes, &f)
@@ -73,11 +76,45 @@ func runAccessToken(t *testing.T, td tokenData) (string, map[string]interface{})
 			t.Errorf("Failed to parse json: %v\ntokenData: %v\nbody: %v", err, td, body)
 		} else {
 			m = f.(map[string]interface{})
+			if len(m) != len(td.bodyJson) {
+				t.Errorf("Reponse doesn't have the same number of keys.  Got: %v  Expected %v\ntokenData: %v", m, td.bodyJson, td)
+			}
+
+			// Loop over the keys/values expected and comapre to what we got
+			for k, tdv := range td.bodyJson {
+				mv, ok := m[k]
+				if !ok {
+					t.Errorf("Reponse missing key %v.  Got: %v  Expected %v\ntokenData: %v", k, m, td.bodyJson, td)
+					continue
+				}
+				t.Logf("testing key %v  value: %v", k, tdv)
+				// don't check values if expected data is nil
+				if tdv == nil {
+					continue
+				}
+				switch tdvv := tdv.(type) {
+				case string:
+					mvv, ok := mv.(string)
+					if !ok || tdvv != mvv {
+						t.Errorf("Types/Values don't match for key %v.  Got: %v  Expected %v\ntokenData: %v", k, m, td.bodyJson, td)
+					}
+				case float64:
+					mvv, ok := mv.(float64)
+					if !ok || tdvv != mvv {
+						t.Errorf("Types/Values don't match for key %v.  Got: %v  Expected %v\ntokenData: %v", k, m, td.bodyJson, td)
+					}
+				default:
+					t.Errorf("Unhandled type for key %v.  Got: %v  Expected %v\ntokenData: %v", k, m, td.bodyJson, td)
+				}
+			}
 		}
 	}
-	// if body != td.body {
-	// 	t.Errorf("Body data does not match, got %v  Expected %v\ntokenData: %v", body, td.body, td)
-	// }
 
 	return body, m
+}
+
+func runProxyAuth(t testing.T) {
+	// curl --header "Authorization: Bearer MmU3ZWI2YzgtMDY3YS00NjM5LTg1MjEtYzcyYzc1NjU3ODEw"
+	// --data "username=takumi&password={SHA256}2QJwb00iyNaZbsEbjYHUTTLyvRwkJZTt8yrj4qHWBTU="
+	// http://localhost:8080/api/2/domains/topcoder.com/proxyauth ; echo
 }
